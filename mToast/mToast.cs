da2998c41cbb614a1883919eb817491cc9f215c4
Daniel Young <6733866+membear@users.noreply.github.com>
2018-06-11 17:35:44 -0400

    Remove redundant include

--- mToast/mToast.cs
+++ mToast/mToast.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Json;
-using System.Text;
 using Windows.UI.Notifications;
 
 using DesktopToast;
